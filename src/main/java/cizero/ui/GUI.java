972fe0ff1fc0d5a329020542800bceadf600b558
Bollpiska <bollpiska@hotmail.com>
2020-02-05 13:45:56 +0100

    lade till klassen testMain.java i gitignore så kan alla ha sin egen för att köra sin kod innan vår riktiga main är klar

--- src/main/java/cizero/ui/GUI.java
+++ src/main/java/cizero/ui/GUI.java
@@ -5,8 +5,29 @@ import java.utils.*;
 public class GUI extends JFrame {
 
   private ContactBook contacts;
+  private JTextArea textArea = new JTextArea();
+  private JLabel bigLabel = new JLabel();
+  private JTextField fNameField = new JTextField();
+  private JTextField lNameField = new JTextField();
+  private JTextField phoneField = new JTextField();
+  private JTextField mailField = new JTextField();
+  private JLabel fNameLabel = new JLabel("Förnamn");
+  private JLabel eNameLabel = new JLabel("Efternamn");
+  private JLabel phoneLabel = new JLabel("telenummer");
+  private JLabel mailLabel = new JLabel("mail");
 
 
+  public GUI(){
+
+    add(bigLabel);
+    add(textArea);
+
+    setVisible(true);
+    setSize(500,300);
+    setDefaultCloseOperation(EXIT_ON_CLOSE;
+
+
+  }
 
 
 }
